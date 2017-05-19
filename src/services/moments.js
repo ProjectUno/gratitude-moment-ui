9dd17165ba01c63cb9bae4f5da4dbf8a0ab3a817
joshhaws <joshhaws0@gmail.com>
2017-05-19 17:21:24 -0600

    fix https: crossbrowser stuff

--- src/services/moments.js
+++ src/services/moments.js
@@ -4,7 +4,7 @@ var helpers = {
   getMoments: () => {
     return axios({
         method: 'get',
-        url: 'http://gratitude-moment.herokuapp.com/moments',
+        url: 'https://gratitude-moment.herokuapp.com/moments',
         headers: {
             'Content-Type': 'application/json',
             'Authorization' : 'Token token=wlyEUvgTW4lZMH5u5b1OHAtt',
@@ -16,7 +16,7 @@ var helpers = {
     })
     .catch(error =>{
       return { status: error }
-    }) 
+    })
   }
 }
 
